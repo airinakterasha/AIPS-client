04ab8c69f4c6b6604163a36b0b6ce5d9ea84e4af
Airin Akter <airinmoni.bd@gmail.com>
2024-05-09 22:54:53 +0600

    added navbar

---
+++ src/routes/Routes.jsx
@@ -0,0 +1,29 @@
+import {createBrowserRouter} from "react-router-dom";
+import Main from "../layout/Main";
+import Home from "../pages/Home/Home";
+import Login from "../pages/Login/Login";
+import Registration from "../pages/Registration/Registration";
+
+const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Main></Main>,
+      children: [
+        {
+            path: "/",
+            element: <Home></Home>
+        },
+        {
+            path: "/login",
+            element: <Login></Login>
+        },
+        {
+            path: "/registration",
+            element: <Registration></Registration>
+        },
+      ]
+    },
+]);
+
+
+export default router
